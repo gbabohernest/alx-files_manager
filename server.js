d5d4ca364cafb26034c69abca597bd856a6867f1
gbabohernest <gbaboh.ernest@gmail.com>
2024-05-15 01:14:27 +0300

    UPDATE server PORT variable definition

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const routes = require('./routes/index');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
 app.use(express.json()); // parse json request
 // app.use('/', routes);
 app.use(routes);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
