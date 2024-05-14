939e8097382b27e67abdf3cb62c7c5c558293f98
gbabohernest <gbaboh.ernest@gmail.com>
2024-05-14 20:31:44 +0300

    Configure Express server to parse JSON requests by adding a express.json() middleware

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./routes/index');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use('/', routes);
+app.use(express.json()); // parse json request
+// app.use('/', routes);
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
